a2d34576e9a8366c805bbbddb7c838a5837f727b
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-08 10:11:34 -0700

    add update

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -9,5 +9,38 @@ class StoriesController < ApplicationController
     render :show
   end
 
+  def new
+    @story = Story.new
+    render :new
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+    render :edit
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to  stories_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @story= Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to stories_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def story_params
+      params.require(:story).permit(:title, :image)
+    end
+
 
 end
