367af1205e8c007c2ab835fba2118cfebd7b2a9f
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-08 16:42:47 -0700

    add index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'stories#index'
+
   resources :stories do
-    resources :sentences
+    resources :sentences, :except => [:show, :index]
   end
 end
