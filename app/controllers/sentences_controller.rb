1112f9b950ca2c33358172e13bca8e098aad593c
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-08 13:30:38 -0700

    add update destroy and edit for Sentences

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,4 +1,5 @@
 class SentencesController < ApplicationController
+
   def new
     @story = Story.find(params[:story_id])
     @sentence = @story.sentences.new
@@ -12,6 +13,27 @@ class SentencesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @story = Story.find(params[:story_id])
+    @sentence = Sentence.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:story_id])
+    @sentence = Sentence.find(params[:id])
+    if @sentence.update(sentence_params)
+      redirect_to story_path(@sentence.story)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sentence = Sentence.find(params[:id])
+    @sentence.destroy
+    redirect_to story_path(@sentence.story)
   end
 
 private
